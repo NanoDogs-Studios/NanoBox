1d38c8d5c16acd115ef9b47c9a35a8a0d0d574f9
Laym <65513977+LaymGlitched@users.noreply.github.com>
2026-02-01 03:27:33 +0000

    modding stuffs

---
+++ Assets/Scripts/NanoBoxMenuModio.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using Modio;
+using Modio.Authentication;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+using Modio.Users;
+using TMPro;
+
+namespace Nanodogs.Nanobox.UI
+{
+    public class NanoBoxMenuModio : MonoBehaviour
+    {
+        [SerializeField] TMP_InputField authInput;
+        [SerializeField] Button authRequest;
+
+        [SerializeField] TMP_InputField codeInput;
+        [SerializeField] Button codeSubmit;
+
+        void Awake()
+        {
+            ModioServices.Bind<IModioAuthService>().FromInstance(new ModioEmailAuthService(GetAuthCode));
+        }
+
+        async Task OnInit()
+        {
+            if (User.Current.IsAuthenticated)
+            {
+                OnAuth();
+                return;
+            }
+    
+            // You can assign these using the Inspector if you prefer
+            authRequest.onClick.AddListener(async () => await Authenticate());
+        }
+   
+        async Task Authenticate()
+        {
+            Error error = await ModioClient.AuthService.Authenticate(true, authInput.text);
+    
+            if (error)
+            {
+                Debug.LogError($"Error authenticating with email: {error}");
+                return;
+            }
+    
+            OnAuth();
+        }
+
+        // This will be called by the ModioEmailAuthService object we constructed earlier
+        async Task<string> GetAuthCode()
+        {
+            bool codeEntered = false;
+    
+            codeSubmit.onClick.AddListener(() => codeEntered = true);
+    
+            while (!codeEntered)
+                await Task.Yield();
+    
+            return authInput.text;
+        }
+   
+        void OnAuth()
+        {
+            Debug.Log($"Authenticated user: {User.Current.Profile.Username}");
+        }
+    }
+}
